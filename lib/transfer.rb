a16bf8d5754f842be3fac093f0c7bfef488f4da6
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-02 03:47:29 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -33,7 +33,7 @@ class Transfer
 
   def reverse_transfer
     if self.status == 'complete'
-      binding.pry
+      #binding.pry
       receiver.balance = receiver.balance - @amount
       sender.balance += @amount
     end
